5e889a4e11449a2f58a6f847fc1412281b032d30
alonehover <puhongwu@live.com>
2015-11-03 18:14:39 +0800

    update

--- template/js/my/myList.js
+++ template/js/my/myList.js
@@ -2,5 +2,12 @@ $(function(){
   $("#sign-out").on('click',function(){
       window.localStorage.removeItem('userInfo');
       window.location.href = '/diguaApp/index.html';
-  })
+  });
+
+  function load(){
+    var code = service.getUserInfo()['code'];
+    $('#recommend_code').html(code);
+  }
+  
+  load();
 })
--- template/js/push/mybusiness.js
+++ template/js/push/mybusiness.js
@@ -3,7 +3,7 @@ $(function(){
         service.singleEmployeeDtCount(function(flag,msg){
             if (flag) {
                 var data = msg.countData.oneDay;
-                $("#recommend_code").html(msg.recommendCode);
+                // $("#recommend_code").html(msg.recommendCode);
                 $("#recordNum").html(data.recordNumber);
                 $("#regNum").html(data.newUser);
                 $("#orderNum").html(data.newOrder);
